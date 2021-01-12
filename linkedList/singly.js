39b49994fa7ac7d1149361d9e6180038e4c8d7c3
Carlos Eduardo Villanueva Altuna <carlos.villanueva.altuna@gmail.com>
2021-01-12 12:18:26 -0300

    [add] singly linked list

--- linkedList/singly.js
+++ linkedList/singly.js
@@ -80,32 +80,85 @@ class MySinglyLinkedList {
 
     return this;
   }
+
+  /**
+   * Metodo para agregar un nodo intermedio
+   * @param {*} index -> donde agregaremos el nuevo nodo
+   * @param {*} value  -> valor que vamos a agregar
+   * @returns
+   */
   insert(index, value) {
+    //si pasamos un indice mayor a la cantidad de elementos en la lista
     if (index >= this.length) {
+      //agregamos el valor del nodo al final de la lista
       return this.append(value);
     }
 
+    if (index === 0) {
+      return this.append(value);
+    }
+
+    //creamos un nuevo node
     const newNode = new Node(value);
+
+    //firstPointer -> es el nodo anterior o previo del indice que pasamos
     const firstPointer = this.getTheIndex(index - 1);
+
+    //holdingPointer -> es el nodo que no queremos que se borre por el GC sino que se quede en hold (espera) hasta puntearlo
     const holdingPointer = firstPointer.next;
+
+    //empezamos a mover lo nodos, el nodo firstPointer apunta al nuevo nodo
     firstPointer.next = newNode;
+
+    //el nuevo nodo apunta al nodo en espera o holding que es el indice donde queremos insertar el nuevo nodo
     newNode.next = holdingPointer;
 
+    //al agregar un nuevo nodo incrementamos la longitud del nuevo nodo
     this.length++;
 
     return this;
   }
 
+  /**
+   * Metodo que permite buscar el índice que pasamos
+   * @param {*} index -> indice de la lista donde queremos agregar el nuevo nodo
+   * @returns
+   */
   getTheIndex(index) {
+    //contador que se actualiza con los nodos que se recorre
     let counter = 0;
+    //nodo actual será siempre el nodo donde estamos posicionados dentro de la lista, empezamos en el head
     let currentNode = this.head;
 
+    //recorremos cada uno de los nodos hasta encontrar el índice que queremos buscar
     while (counter !== index) {
+      //actualizamos el nodo actual
       currentNode = currentNode.next;
       counter++;
     }
 
+    //retornamos el nodo actual
     return currentNode;
+  }
+
+  remove(index) {
+    if (index >= this.length) {
+      console.error("Index is out of limits of the array");
+    } else if (index === 0) {
+      this.head = this.head.next;
+      this.length--;
+    } else if (index === this.length - 1) {
+      const firstPointer = this.getTheIndex(index - 1);
+      firstPointer.next = null;
+
+      this.tail = firstPointer;
+      this.length--;
+    } else {
+      const firstPointer = this.getTheIndex(index - 1);
+      const pointerToRemove = firstPointer.next;
+      firstPointer.next = pointerToRemove.next;
+      this.length--;
+    }
   }
 }
 
